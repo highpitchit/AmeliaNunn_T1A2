2eb5f71f997495e52537659315128f8245794fec
highpitchit <ameliafrancesnunn@gmail.com>
2020-03-11 21:46:41 +1000

    random questions working wo repeat, loop through 3 questions

--- src/classes/1_beginner.rb
+++ src/classes/1_beginner.rb
@@ -1,75 +1,82 @@
 #addition and subtraction
 #can count up to 1000
+require "colorize"
 
 class Quiz
     def initialize(set)
         @set_of_quizzes = set
+        @correct_answer = true
     end
 
-    def ask_question
+    def ask_questions 
         # puts random :question from @set_of_quizzes
+        questions_answered = 0
 
+        while (correct_answer and questions_answered < 3) do
+
+            random_number = @set_of_quizzes.to_a.sample()      #randomly select hash to use from beginner_quizzes and turns it to array
+            z = [Hash[*random_number.flatten]]                  #changes random back to hash
+
+            puts n1 = z.map {|x| x.values[0]}       #generates the question
+                n2 = z.map {|x| x.values[1]}       #generates the corresponding answer
+            answer = gets.chomp
+
+            if (n2[0] == answer.to_i)
+                puts ("Correct").colorize(:yellow)
+                @set_of_quizzes.delete_if {|a| a[:question_text] == n1[0] }
+            else 
+                puts "Incorrect".colorize(:red)
+                @correct_answer = false
+            end
+
+            questions_answered +=1
+
+        end
+    end
+
+    def correct_answer
+        return @correct_answer
     end
 end
 
 
-# class BeginnerLevel 
-#     attr_accessor :b_q
-    
-#     def initialize
-#         @sr = SimpleRandom.new
-#     end
-    
-#     def beginner_q  
-#       @b_q = {
-#         :b_q1 => "If you bought 1 apple and 1 orange, how many pieces of fruit would you have?", 
-#         :b_q2 => "Now you need 3 potatos, 2 broccoli and 4 zuchinni. What is the total number of items added to your basket?",
-#         :b_q3 => "The shopping list has asked for 6 bananas, but you have picked up a bunch with 8. How many bananas should you put back?",
-#         :b_q4 => "If you bought 2 pears and 1 lemon, how many pieces of fruit would you have?", 
-#         :b_q5 => "Now you need 1 loaf of bread, 2 broccoli and 3 mushrooms. What is the total number of items added to your basket?",
-#         :b_q6 => "The shopping list has asked for 13 bananas, but you have picked up a bunch with 8. How many more bananas do you need?",    
-#         :b_q7 => "If you bought 1 potato and 3 limes, how many pieces of fruit would you have?", 
-#         :b_q8 => "Now you need 3 corn cobs, 10 carrots and 1 avocado. What is the total number of items added to your basket?",
-#         :b_q9 => "The shopping list has asked for 1 banana, but you have picked up a bunch with 9. How many bananas should you put back?", 
-#         :b_q10 => "If you bought 2 peach and 5 oranges, how many pieces of fruit would you have?", 
-#         :b_q11 => "Now you need 6 bread rolls, 4 apples and 7 lemons. What is the total number of items added to your basket?",
-#         :b_q12 => "The shopping list has asked for 12 grapes, but you have picked up a bunch with 36. How many bananas should you put back?"
-#     }
-#     end
-#     def beginner_a  
-#       @b_a = {
-#         :b_a1 => 2, 
-#         :b_a2 => 9,
-#         :b_a3 => 2,
-#         :b_a4 => 3,
-#         :b_a5 => 6,
-#         :b_a6 => 5,
-#         :b_a7 => 4,
-#         :b_a8 => 14,
-#         :b_a9 => 8,
-#         :b_a10 => 7,
-#         :b_a11 => 17,
-#         :b_a12 => 24,
-# }
-#     end
-# end
-
 beginner_quizzes = [{
-    question_text: "If you bought 1 apple and 1 orange, how many pieces of fruit would you have?",
-    answer_value: 2
-}, {
+  question_text: "If you bought 1 apple and 1 orange, how many pieces of fruit would you have?",
+  answer_value: 2
+  },
+  {question_text: "If you need 3 potatos, 2 broccoli and 4 zuchinni. What is the total number of items you added to your basket?",
+  answer_value: 9
+  },
+  {question_text: "The shopping list has asked for 6 bananas, but you have picked up a bunch with 8. How many bananas should you put back?",
+  answer_value: 2
+  },
+  {question_text:  "Now you need 1 loaf of bread, 2 broccoli and 3 mushrooms. What is the total number of items added to your basket?", answer_value: 6},
+  {question_text:  "If you bought 2 pears and 1 lemon, how many pieces of fruit would you have?", answer_value: 3},
+  {question_text:   "The shopping list has asked for 13 bananas, but you have picked up a bunch with 8. How many more bananas do you need?", answer_value: 5 
+  }]
 
+intermediate_quizzes = [{
+    question_text: "If you boughtdasadasd 1 apple and 1 orange, how many pieces of fruit would you have?",
+    answer_value: 2
+},
+{question_text: "If you need 3 adsdasdad, 2 broccoli and 4 zuchinni. What is the total number of items you added to your basket?",
+    answer_value: 9
+},
+{question_text: "The shopping adsasdasdasd has asked for 6 bananas, but you have picked up a bunch with 8. How many bananas should you put back?",
+    answer_value: 2
 }]
 
 bqs = Quiz.new(beginner_quizzes)
-iqs = Quiz.new(intermediate_quizzes
+bqs.ask_questions
 
-bqs.beginner_quizzes(question_text:)
+puts (bqs.correct_answer == true) ? "Congratulations you finished the level" : exit
 
-    # puts q1 = bqs.beginner_q[:b_q1]
-# a1 = gets.chomp.to_i
-# ca1 = bqs.beginner_a[:b_a1]
 
-# puts  a1 == ca1 ? "Correct!" : "Incorrect" 
+iqs = Quiz.new(intermediate_quizzes)
+iqs.ask_questions
 
-# puts rand(5)
+# iqs = Quiz.new(intermediate_quizzes)
+
+
+
+   
